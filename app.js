b6f6565a0a71b56a36ddc2e6a8c67b9d1faaf083
jimmyhuynh2000 <jimmyhuynh2000@vt.edu>
2022-07-01 22:14:21 -0400

    Server up to date

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 4999
 
-const server = express()
+const app = express()
 const publicPath = path.join(__dirname, 'build')
 
- const p5Path = path.join(__dirname,'src');
-server.use(express.static(publicPath))
+app.use(express.static(publicPath))
 
-
-server.get("/*", (req, res)=>{
+app.get("/", (req, res)=>{
     res.sendFile(path.join(publicPath, 'index.html'))
 })
+app.get("/*", (req, res)=>{
+    res.send("Page is Missing")
+})
 
-server.listen(port, (err)=>{
+
+
+app.listen(port, (err)=>{
     if (err) {
         console.log(err)
         process.exit(1)
