dd52d2e67411e2f12919e231fdc600b4d4294e54
jimmyhuynh2000 <jimmyhuynh2000@vt.edu>
2022-07-01 22:32:53 -0400

    Style changes

--- app.js
+++ app.js
@@ -7,13 +7,9 @@ const publicPath = path.join(__dirname, 'build')
 
 app.use(express.static(publicPath))
 
-app.get("/", (req, res)=>{
+app.get("/*", (req, res)=>{
     res.sendFile(path.join(publicPath, 'index.html'))
 })
-app.get("/*", (req, res)=>{
-    res.send("Page is Missing")
-})
-
 
 
 app.listen(port, (err)=>{
